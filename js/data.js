13c80def309223b5f01288c0d6962d927e9c58f9
Shaun Wahle <YOUR EMAIL ADDRESS>
2017-07-12 08:27:03 -0700

    Adding notes and js logic

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 'use strict';
-
+//adding raw data onjects
 var rawData = [
   {
-    projectName: 'States Game',
+    projectName: 'Sweat It Out',
     description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     siteUrl: 'www.google.com',
     repoUrl: 'www.bing.com'
--- js/project.js
+++ js/project.js
@@ -1,11 +1,12 @@
 'use strict';
-
+//Reworked hamburger button for a click function. Toggle will set display to block and none
 $('.icon-menu').on('click', function() {
   $('nav').toggle();
 });
-
+//declaring global variable
 var projects = [];
 
+//constructor function building out objects
 function Project(rawDataObj) {
   this.projectName = rawDataObj.projectName;
   this.description = rawDataObj.description;
@@ -13,6 +14,8 @@ function Project(rawDataObj) {
   this.repoUrl = rawDataObj.repoUrl;
 }
 
+//prototype function that creates a deeo copy stored as $newProject, removes the class of template
+//finds the decendants of project-title-overlay and adds projectName attribute to it. Finally returning the var that we built
 Project.prototype.toHtml = function() {
   var $newProject = $('article.template').clone();
 
@@ -23,10 +26,11 @@ Project.prototype.toHtml = function() {
   return $newProject;
 };
 
+//this will loop through my raw data and forEach it will push the Project objects
 rawData.forEach(function(projectObject) {
   projects.push(new Project(projectObject));
 });
-
+//finall this will append each object to my id projects in html format
 projects.forEach(function(project) {
   $('#projects').append(project.toHtml());
 });
