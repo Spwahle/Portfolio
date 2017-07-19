bf929cc6033a1b68bd211f234e69984d5bfb5d87
Shaun Wahle <YOUR EMAIL ADDRESS>
2017-07-19 10:48:59 -0700

    Server working

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 'use strict';
 
- let express = require('express');
- let app = express();
-  const bodyParser = require('body-parser').urlencoded({extended: true});
-  const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = process.env.PORT || 3000;
 
 
  app.use(express.static('./public'));
 
 
 
- @@ -23,5 +27,6 @@ app.post('/articles', bodyParser, function(request, response) {
+ app.post('/project', bodyParser, function(request, response) {
   })
 
   app.listen(PORT, function() {
   console.log(`Listening on port: "${PORT}"`);
   });
+  'use strict';
+
+  // const express = require('express');
+  // const app = express();
+  // const PORT = process.env.Port || 8080;
+  //
+  // app.use(express.static('./public'));
+  //
+  // app.get('/', function(req, rest) {
+  //   console.log('whats up');
+  //   res.sendFile('./index.html');
+  // });
+  //
+  // app.get('/new', function(req, rest) {
+  //   res.SendFile('./new.html', {root: './public'});
+  // });
+  //
+  // app.listen(PORT, function() {
+  //   console.log('Current Port Number:', PORT);
+  // });
