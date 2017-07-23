20e74fa52f116cf13b0b3e9a8ab8109c6d725957
Shaun Wahle <YOUR EMAIL ADDRESS>
2017-07-23 11:52:21 -0700

    Just little updates

--- server.js
+++ server.js
@@ -16,23 +16,3 @@ const PORT = process.env.PORT || 3000;
   app.listen(PORT, function() {
   console.log(`Listening on port: "${PORT}"`);
   });
-  'use strict';
-
-  // const express = require('express');
-  // const app = express();
-  // const PORT = process.env.Port || 8080;
-  //
-  // app.use(express.static('./public'));
-  //
-  // app.get('/', function(req, rest) {
-  //   console.log('whats up');
-  //   res.sendFile('./index.html');
-  // });
-  //
-  // app.get('/new', function(req, rest) {
-  //   res.SendFile('./new.html', {root: './public'});
-  // });
-  //
-  // app.listen(PORT, function() {
-  //   console.log('Current Port Number:', PORT);
-  // });
