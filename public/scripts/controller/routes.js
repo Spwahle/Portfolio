a7eea4faa17124912674cc0bf45686c5476cdea6
Shaun Wahle <YOUR EMAIL ADDRESS>
2017-07-31 07:30:50 -0700

    Npm install

--- public/scripts/controller/routes.js
+++ public/scripts/controller/routes.js
@@ -7,3 +7,29 @@ page('/about', app.aboutController.index);
 page('/home', app.Project.fetchAll);
 
 page();
+
+
+
+var my_handlers = function (nodes) {
+var helper = function (i) {
+  return function (e) {
+    alert(i);
+  };
+};
+var x;
+for (x = 0; x < nodes.length; x++) {
+  jQuery(nodes[x]).on(‘click’, helper(x));
+}
+};
+
+var my_handlers = function (nodes) {
+  var helper = function (i) {
+    return function (e) {
+      alert(i);
+    };
+  };
+  var x;
+  for (x = 0; x < nodes.length; x++) {
+    jQuery(nodes[x]).on(‘click’, helper(x));
+  }
+};
